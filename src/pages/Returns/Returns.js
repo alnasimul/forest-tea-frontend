8bf86cb755ca0dcc0f38220da93e454de117cf38
Al Nasimul Haque <alnasimul@gmail.com>
2022-02-01 00:43:19 +0600

    pushed again

--- src/pages/Returns/Returns.js
+++ src/pages/Returns/Returns.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import Layout from "../../components/Layout/Layout";
 import ReturnsTable from "../../components/Returns/ReturnsTable/ReturnsTable";
 
@@ -6,6 +7,7 @@ const Returns = () => {
   return (
     <Layout>
       <div className="w-full mt-5  sm:px-5 bg-white rounded-lg text-center lg:mt-20 md:mr-5">
+        <ToastContainer/>  
         <div className="container">
           <ReturnsTable />
         </div>
